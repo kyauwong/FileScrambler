90c2e40f2d132f61e7175270a42574707a99fb41
Kaven <Kaven@Kaven-laptop>
2016-12-29 19:51:47 +0100

    Implemented class to collect all tests from QtTest

---
+++ UnitTests/TestCollector.h
@@ -0,0 +1,44 @@
+/* BASED ON
+ * http://qtcreator.blogspot.de/2009/10/running-multiple-unit-tests.html
+ */
+
+#ifndef TESTCOLLECTOR_H
+#define TESTCOLLECTOR_H
+
+#include <QtTest>
+#include <memory>
+#include <map>
+#include <string>
+
+namespace TestCollector{
+typedef std::map<std::string, std::shared_ptr<QObject> > TestList;
+inline TestList& GetTestList()
+{
+   static TestList list;
+   return list;
+}
+
+inline int RunAllTests(int argc, char **argv) {
+    int result = 0;
+    for (const auto&i:GetTestList()) {
+        result += QTest::qExec(i.second.get(), argc, argv);
+    }
+    return result;
+}
+
+template <class T>
+class UnitTestClass {
+public:
+    UnitTestClass(const std::string& pTestName) {
+        auto& testList = TestCollector::GetTestList();
+        if (0==testList.count(pTestName)) {
+            testList.insert(std::make_pair(pTestName, std::make_shared<T>()));
+        }
+    }
+};
+}
+
+#define ADD_TEST(className) static TestCollector::UnitTestClass<className> \
+    test(#className);
+
+#endif // TESTCOLLECTOR_H
